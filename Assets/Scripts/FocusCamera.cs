24bb658d50774b641da8898d6ef23a27419a30e0
zahrafayya <zahra.fayyadiyati@gmail.com>
2023-01-06 20:54:49 +0700

    feat: add UI

---
+++ Assets/Scripts/FocusCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Vuforia;
+
+public class FocusCamera : MonoBehaviour
+{
+    void Start()
+    {
+        OnSingleTapped();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        OnSingleTapped();
+    }
+
+    private void OnSingleTapped()
+    {
+        TriggerAutoFocus();
+    }
+    
+    public void TriggerAutoFocus()
+    {
+        StartCoroutine(EnableContinuousFocusIfSet());
+    }
+
+    private IEnumerator EnableContinuousFocusIfSet()
+    {
+        yield return new WaitForSeconds(0.5f);
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+}
---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public GameObject menuPanel;
+
+    private bool _isOpen;
+
+    public void OnButtonClick()
+    {
+        _isOpen = !_isOpen;
+        _toggleMenuPanel();
+    }
+
+    private void _toggleMenuPanel()
+    {
+        if (!_isOpen) menuPanel.SetActive(false);
+        else menuPanel.SetActive(true);
+    }
+}
