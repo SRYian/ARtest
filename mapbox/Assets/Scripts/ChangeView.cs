15d3e0af1b0bb1f63edd986be1ec6796101534f6
zahrafayya <zahra.fayyadiyati@gmail.com>
2023-01-07 22:53:46 +0700

    feat: add UI for LihatPeta and LihatKamera

---
+++ mapbox/Assets/Scripts/ChangeView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeView : MonoBehaviour
+{
+    public GameObject lihatPeta;
+    public GameObject lihatKamera;
+
+    private bool _currentViewIsPeta;
+
+    public void OnButtonClick()
+    {
+        _currentViewIsPeta = !_currentViewIsPeta;
+        _changeMode();
+    }
+
+    private void _changeMode()
+    {
+        if (_currentViewIsPeta)
+        {
+            lihatPeta.SetActive(false);
+            lihatKamera.SetActive(true);
+        }
+        else
+        {
+            lihatPeta.SetActive(true);
+            lihatKamera.SetActive(false);
+        }
+    }
+}
